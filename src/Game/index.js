5cd6f019c321c13f7ffd24f707e6eba13f0d0749
Paulina Korpak <korpak.paulina@gmail.com>
2021-03-29 20:25:39 +0100

    Add restart button

--- src/Game/index.js
+++ src/Game/index.js
@@ -4,6 +4,9 @@ export const Game = (element) => {
   const init = (rows, cols) => {
     const boardElement = element.querySelector('.board');
     Board(boardElement, showResult).init(rows, cols);
+
+    const restartButton = element.querySelector('.result button');
+    restartButton.addEventListener('click', restart);
   };
 
   const showResult = () => {
@@ -13,5 +16,10 @@ export const Game = (element) => {
     table.classList.add('d-none');
     resultElement.classList.remove('d-none');
   };
+
+  const restart = () => {
+    window.location.reload(true);
+  };
+
   return { init };
 };
